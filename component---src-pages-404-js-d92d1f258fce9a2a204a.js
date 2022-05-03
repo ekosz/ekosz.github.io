35b66a64fc75e48af178ec5b502a864c3b032741
ekosz <ekosz@users.noreply.github.com>
2022-05-03 00:10:30 +0000

    deployed via Gatsby Publish Action 🎩 for 813aa89b6c7c2ca723a9ddc54cb4ec98ad89214a

---
+++ component---src-pages-404-js-d92d1f258fce9a2a204a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkekosz_github_com=self.webpackChunkekosz_github_com||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),o=a(8678),u=a(262);e.default=function(t){var e=t.data,a=t.location,l=e.site.siteMetadata.title;return n.createElement(o.Z,{location:a,title:l},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d92d1f258fce9a2a204a.js.map
